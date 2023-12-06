6c184a3548c738a568c791550f021fb0ed0844e5
Max Worsley <maxworsley@outlook.com>
2023-12-06 14:45:51 +0000

    Update project template

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-/// Main binary function, responsible for:
-/// * Calling command line parsing logic
-/// * Setting up configuration
-/// * Calling the run function in lib.rs
-/// * Handling the error if the above returns an error
 pub fn main() {
-    rust_template::run();
+    println!("Hello, world!");
 }
