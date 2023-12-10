6ffa116247ff3a8bb3b90d837e130309a76dbe49
Max Worsley <maxworsley@outlook.com>
2023-12-10 22:42:36 +0000

    Fix key schedule with transpose

--- src/aes/key_schedule.rs
+++ src/aes/key_schedule.rs
@@ -1,7 +1,7 @@
 use crate::aes::block::{make_empty_block, Block};
 use crate::aes::sub_bytes::get_substitute_byte;
 
-pub fn generate_key_schedule(key: &Block) -> Vec<Block> {
+pub fn generate_key_schedule_old(key: &Block) -> Vec<Block> {
     let mut key_words = vec![];
     key_words.push(key[0].clone());
     key_words.push(key[1].clone());
@@ -30,6 +30,53 @@ pub fn generate_key_schedule(key: &Block) -> Vec<Block> {
             key_words[block_start_i + 2].clone(),
             key_words[block_start_i + 3].clone(),
         ])
+    }
+    return_blocks
+}
+
+pub fn generate_key_schedule(key: &Block) -> Vec<Block> {
+    let mut key_words = vec![];
+
+    for i in 0..key.len() {
+        let mut new_row = vec![];
+        for j in 0..key[i].len() {
+            new_row.push(key[j][i]);
+        }
+        key_words.push(new_row);
+    }
+
+    for i in 0..10 {
+        let prev_i_start = i * 4;
+        let malformed_prev = malform_vector(&key_words[prev_i_start + 3], i + 1);
+        let w0 = xor_vec(&key_words[prev_i_start], &malformed_prev);
+        let w1 = xor_vec(&key_words[prev_i_start + 1], &w0);
+        let w2 = xor_vec(&key_words[prev_i_start + 2], &w1);
+        let w3 = xor_vec(&key_words[prev_i_start + 3], &w2);
+        key_words.push(w0);
+        key_words.push(w1);
+        key_words.push(w2);
+        key_words.push(w3);
+    }
+
+    let mut return_blocks = vec![];
+    for i in 0..11 {
+        let block_start_i = i * 4;
+        let untrasposed = vec![
+            key_words[block_start_i].clone(),
+            key_words[block_start_i + 1].clone(),
+            key_words[block_start_i + 2].clone(),
+            key_words[block_start_i + 3].clone(),
+        ];
+        let mut transposed_block = vec![];
+
+        for i in 0..untrasposed.len() {
+            let mut new_row = vec![];
+            for j in 0..untrasposed[i].len() {
+                new_row.push(untrasposed[j][i]);
+            }
+            transposed_block.push(new_row);
+        }
+        return_blocks.push(transposed_block);
     }
     return_blocks
 }
@@ -83,5 +130,37 @@ mod tests {
         let result = xor_vec(&data_a, &data_b);
 
         assert_eq!(result, exp_result);
+    }
+
+    #[test]
+    fn test_key_expansion() {
+        let mut key = vec![
+            vec![0x2b, 0x28, 0xab, 0x09],
+            vec![0x7e, 0xae, 0xf7, 0xcf],
+            vec![0x15, 0xd2, 0x15, 0x4f],
+            vec![0x16, 0xa6, 0x88, 0x3c],
+        ];
+        let first_expanded_key = vec![
+            vec![0x2b, 0x28, 0xab, 0x09],
+            vec![0x7e, 0xae, 0xf7, 0xcf],
+            vec![0x15, 0xd2, 0x15, 0x4f],
+            vec![0x16, 0xa6, 0x88, 0x3c],
+        ];
+        let second_expanded_key = vec![
+            vec![0xa0, 0x88, 0x23, 0x2a],
+            vec![0xfa, 0x54, 0xa3, 0x6c],
+            vec![0xfe, 0x2c, 0x39, 0x76],
+            vec![0x17, 0xb1, 0x39, 0x05],
+        ];
+        let third_expanded_key = vec![
+            vec![0xf2, 0x7a, 0x59, 0x73],
+            vec![0xc2, 0x96, 0x35, 0x59],
+            vec![0x95, 0xb9, 0x80, 0xf6],
+            vec![0xf2, 0x43, 0x7a, 0x7f],
+        ];
+        let keys_generated = generate_key_schedule(&mut key);
+        assert_eq!(keys_generated[0], first_expanded_key);
+        assert_eq!(keys_generated[1], second_expanded_key);
+        assert_eq!(keys_generated[2], third_expanded_key);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,65 @@
 mod aes;
+use aes::{
+    key_schedule,
+    add_round_key,
+    shift_rows,
+    mix_columns,
+    sub_bytes,
+    block
+};
+
+pub fn encrypt(data: &mut block::Block, key: &block::Block) {
+    let keys = key_schedule::generate_key_schedule(&key);
+    let mut key_iter = keys.iter();
+    
+    if let Some(key_block) = key_iter.next() {
+        add_round_key::add_round_key(data, &key_block);
+    }
+    print_as_block(&data);
+
+    for _ in 0..1 {
+        sub_bytes::sub_bytes(data);
+        print_as_block(&data);
+        shift_rows::shift_rows(data);
+        print_as_block(&data);
+        mix_columns::mix_columns(data);
+        if let Some(key_block) = key_iter.next() {
+            add_round_key::add_round_key(data, &key_block);
+        }
+        print_as_block(&data)
+    }
+    sub_bytes::sub_bytes(data);
+    shift_rows::shift_rows(data);
+    if let Some(key) = key_iter.next() {
+        add_round_key::add_round_key(data, &key);
+    }
+}
+
+fn print_as_block(data: &block::Block) {
+    for row in data {
+        for element in row {
+            print!("{:x} ", element)
+        }
+        println!();
+    }
+    println!();
+}
 
 pub fn main() {
-    let key = aes::block::make_empty_block();
-    let mut data = aes::block::make_empty_block();
-    data[0][1] = 0x01;
+    let mut data = vec![
+        vec![0x32, 0x88, 0x31, 0xe0],
+        vec![0x43, 0x5a, 0x31, 0x37],
+        vec![0xf6, 0x30, 0x98, 0x07],
+        vec![0xa8, 0x8d, 0xa2, 0x34],
+    ];
 
-    aes::key_schedule::generate_key_schedule(&key);
-    aes::add_round_key::add_round_key(&mut data, &key);
-    aes::mix_columns::mix_columns(&mut data);
-    aes::shift_rows::shift_rows(&mut data);
-    aes::sub_bytes::sub_bytes(&mut data);
+    let key = vec![
+            vec![0x2b, 0x28, 0xab, 0x09],
+            vec![0x7e, 0xae, 0xf7, 0xcf],
+            vec![0x15, 0xd2, 0x15, 0x4f],
+            vec![0x16, 0xa6, 0x88, 0x3c],
+        ];
 
-    println!("{:?}", data);
+    encrypt(&mut data, &key);
+    print_as_block(&data)
 }
